e5e15aadca76a21b3f3e74011d80130c3ed3ba12
Steve Trewick <steve.trewick@icloud.com>
2025-07-11 17:44:46 +0100

    packagey thangs

---
+++ Sources/AACStreamTools/AACCompressor.swift
@@ -0,0 +1,6 @@
+import Foundation
+import AVFoundation
+
+public class AACCompressor {
+  
+}
--- Tests/AACStreamToolsTests/AACStreamToolsTests.swift
+++ Tests/AACStreamToolsTests/AACStreamToolsTests.swift
@@ -1,11 +1,11 @@
 import XCTest
 @testable import AACStreamTools
-
-final class AACStreamToolsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AACStreamTools().text, "Hello, World!")
-    }
-}
+//
+//final class AACStreamToolsTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(AACStreamTools().text, "Hello, World!")
+//    }
+//}
